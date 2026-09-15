960bdb9c54f48fb0abb1d335ee56bf9798273fb5
minhtam3010050 <minhtam3010050@gmail.gmailcom>
2026-09-15 22:42:58 +0700

    Hoan thien cau truc du an quan ly khach san

---
+++ src/test/java/com/qlkhachsan/QlkhachsanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlkhachsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlkhachsanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
